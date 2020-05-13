af92395829cccb948c5df6ada711a6e6d523c551
袁方 <fang.yuan@baishancloud.com>
2020-05-13 09:56:06 +0800

    binary_search

--- binary_search.py
+++ binary_search.py
@@ -39,6 +39,87 @@ def binary_search_rc(arr, low, high, value):
         return mid
 
 
+def binary_search_first_le(arr, value):
+    """查找第一个大于等于给定值的元素"""
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] < value:
+            low = mid + 1
+        elif arr[mid] >= value:
+            if mid == 0 or arr[mid - 1] < value:
+                return mid
+            else:
+                high = mid - 1
+
+    return -1
+
+
+def binary_search_last_se(arr, value):
+    """查找最后一个小于等于给定值的元素"""
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] <= value:
+            if mid == len(arr) - 1 or arr[mid + 1] > value:
+                return mid
+            else:
+                low = mid + 1
+        else:
+            high = mid -1
+
+    return -1
+
+
+def binary_search_first_equal(arr, value):
+    """查找第一个值为value的元素索引"""
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] < value:
+            low = mid + 1
+        elif arr[mid] > value:
+            high = mid -1
+        else:
+            if mid == 0 or arr[mid - 1] != value:
+                return mid
+            else:
+                high = mid - 1
+
+    return -1
+
+
+def binary_search_last_equal(arr, value):
+    """查找最后一个值为value的元素索引"""
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] < value:
+            low = mid + 1
+        elif arr[mid] > value:
+            high = mid - 1
+        else:
+            if mid == len(arr) - 1 or arr[mid + 1] != value:
+                return mid
+            else:
+                low = mid + 1
+
+    return -1
+
+
+def search_bound(arr, num):
+    """查找上下界"""
+    low_bound = binary_search_first_equal(arr, num)
+    up_bound= binary_search_last_equal(arr, num)
+    return [low_bound, up_bound]
+
+
+def test_search_bound():
+    arr = [1, 2, 3, 4, 5, 5, 5, 6, 6, 6, 7]
+    num = 5
+    print(search_bound(arr, num))
+
+
 def test_binary_search():
     arr = list(3 * x for x in range(100))
     print(arr[9])
@@ -51,6 +132,19 @@ def test_binary_search_rescursive():
     print(binary_search(arr, 0))
 
 
+def test_binary_search_first_le():
+    arr = [2, 3, 3, 3, 4, 4, 6, 6, 9]
+    print(binary_search_first_le(arr, 9))
+
+
+def test_binary_search_first_be():
+    arr = [2, 3, 3, 3, 4, 4, 6, 6, 9]
+    print(binary_search_last_se(arr, 8))
+
+
 if __name__ == "__main__":
     # test_binary_search()
-    test_binary_search_rescursive()
+    # test_binary_search_rescursive()
+    # test_binary_search_first_le()
+    # test_binary_search_first_be()
+    test_search_bound()
