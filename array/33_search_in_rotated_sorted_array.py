db8929c042cfa9b7843bc723a9c67c43c3932aed
袁方 <fang.yuan@baishancloud.com>
2020-05-15 19:39:07 +0800

    binary_search_in_rotated_sorted_array

---
+++ array/33_search_in_rotated_sorted_array.py
@@ -0,0 +1,27 @@
+# link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+# 得反复磨这个逻辑，虽然实现代码很简洁，但是逻辑很烧脑
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
+
+
+
