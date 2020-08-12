82352c1f668744005a39391b27c77d8e0f10015a
袁方 <fang.yuan@baishancloud.com>
2020-08-12 09:49:02 +0800

    stack: 232_implement_queue_using_stack

---
+++ stack/232_implement_queue_using_stack.py
@@ -0,0 +1,41 @@
+class MyQueue1:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self._s1 = []
+        self._s2 = []
+        self._front = -1
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        if not self._s1:
+            self._front = x
+        self._s1.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if not self._s2:
+            while self._s1:
+                self._s2.append(self._s1.pop())
+        return self._s2.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if self._s2:
+            return self._s2[-1]
+
+        return self._front
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not (self._s1 or self._s2)
