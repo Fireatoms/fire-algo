b91682f6417288f387d0af076bb81ab96562fcf1
袁方 <fang.yuan@baishancloud.com>
2020-08-18 21:38:29 +0800

    2_add_two_numbers: iteration

--- linkedList/2_add_two_numbers.py
+++ linkedList/2_add_two_numbers.py
@@ -40,3 +40,27 @@ class Solution:
             cur = cur.next
 
         return dummy.next
+
+
+class Solution1:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        cur = dummy
+        carry = 0
+
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            sum = carry + x + y
+            carry = sum // 10
+            cur.next = ListNode(sum % 10)
+            cur = cur.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        if carry:
+            cur.next = ListNode(carry)
+
+        return dummy.next
