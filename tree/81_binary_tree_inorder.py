6ff18ea07db2b04b63d4c319a09d0084def3727b
袁方 <fang.yuan@baishancloud.com>
2020-08-25 22:05:56 +0800

    81_binary_tree_inorder: iteration

--- tree/81_binary_tree_inorder.py
+++ tree/81_binary_tree_inorder.py
@@ -5,11 +5,13 @@
 from typing import List
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
         res = []
@@ -22,3 +24,17 @@ class Solution:
         self.inorder_r(root.left, res)
         res.append(root.val)
         self.inorder_r(root.right, res)
+
+    def inorder_iteration(self, root):
+        res = []
+        stack = []
+        while root or stack:
+            # put all left nodes into stack
+            while root:
+                stack.append(root)
+                root = root.left
+            node = stack.pop()
+            res.append(node.val)
+            # begin with right node
+            root = node.right
+        return res
