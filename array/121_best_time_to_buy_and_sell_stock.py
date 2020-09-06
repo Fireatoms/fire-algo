68068d5329f7bae50fe45a6bac87800ce756ca0f
袁方 <fang.yuan@baishancloud.com>
2020-09-06 16:45:42 +0800

    121_best_time_to_buy_and_sell_stock

---
+++ array/121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,24 @@
+# link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+class Solution:
+    def maxProfitBrute(self, prices: List[int]) -> int:
+        max_profit = 0
+        for i in range(len(prices)-1):
+            for j in range(i+1, len(prices)):
+                cur_profit =  prices[j] - prices[i]
+                if cur_profit > max_profit:
+                    max_profit = cur_profit
+
+        return max_profit
+
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+
+        return max_profit
