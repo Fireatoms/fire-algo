def3a39d5793aecfe698ed106960b35dc60baff3
袁方 <fang.yuan@baishancloud.com>
2020-09-13 17:49:11 +0800

    repetitious: 189_rotate_array_1.py

---
+++ repetitious/189_rotate_array_1.py
@@ -0,0 +1,19 @@
+# link: https://leetcode.com/problems/rotate-array/
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        k %= length
+        self.reverse(nums, 0, length-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, length-1)
+
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
