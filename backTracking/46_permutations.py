d2c1cbe8f6c408c6ec70b4853e1934b04919438f
袁方 <fang.yuan@baishancloud.com>
2020-10-02 01:29:29 +0800

    46_permutations: modify

--- backTracking/46_permutations.py
+++ backTracking/46_permutations.py
@@ -4,29 +4,20 @@ from typing import List
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = []
-        n = k = len(nums)
-        self.permute_r(result, nums, n, k)
+        k = len(nums)
+        self.permute_r(result, nums, k)
 
         return result
 
-    def permute_r(self, result, nums, n, k):
+    def permute_r(self, result, nums, k):
         if k == 1:
             # copy这里精髓了
             result.append(nums.copy())
 
         for i in range(k):
-            tmp = nums[i]
-            nums[i] = nums[k-1]
-            nums[k-1] = tmp
-
-            self.permute_r(result, nums, n, k-1)
-
-            # 复原，也可以直接使用最上面的交换命令再操作一遍
-            # tmp = nums[i]
-            # nums[i] = nums[k - 1]
-            # nums[k - 1] = tmp
-            nums[k-1] = nums[i]
-            nums[i] = tmp
+            nums[i], nums[k-1] = nums[k-1], nums[i]
+            self.permute_r(result, nums, k-1)
+            nums[i], nums[k-1] = nums[k-1], nums[i]
 
 
 if __name__ == "__main__":
