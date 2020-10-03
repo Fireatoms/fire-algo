20c2f69985dc2724f3fa2e95d21f904861fd9cf0
袁方 <fang.yuan@baishancloud.com>
2020-10-03 12:06:05 +0800

    spe: 704_binary_search

---
+++ china_leetcode/spe/704_binary_search.py
@@ -0,0 +1,16 @@
+# link: https://leetcode-cn.com/problems/binary-search/
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] > target:
+                high = mid - 1
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                return mid
+
+        return -1
