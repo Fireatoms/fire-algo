a622e7c553f106ac359f39c678a2333790195504
袁方 <fang.yuan@baishancloud.com>
2020-10-06 08:11:49 +0800

    spe: 145_binary_tree_postorder_traversal: no queue

--- china_leetcode/spe/145_binary_tree_postorder_traversal.py
+++ china_leetcode/spe/145_binary_tree_postorder_traversal.py
@@ -21,3 +21,17 @@ class Solution:
             if node.right: stack.append(node.right)
 
         return list(ans)
+
+    def postorderTraversalNoQueue(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+
+        stack, ans = [root], []
+        while stack:
+            node = stack.pop()
+            ans.append(node.val)
+            if node.left: stack.append(node.left)
+            if node.right: stack.append(node.right)
+
+        ans.reverse()
+        return ans
