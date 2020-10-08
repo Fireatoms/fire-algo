e1afb02fc105feaf44e16688c43bcfbfb1dd9405
袁方 <fang.yuan@baishancloud.com>
2020-10-08 10:46:03 +0800

    new_coder: 136_single_number

---
+++ new_coder/136_single_number.py
@@ -0,0 +1,9 @@
+# link: https://leetcode-cn.com/problems/single-number/
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for num in nums:
+            ans ^= num
+        return ans
