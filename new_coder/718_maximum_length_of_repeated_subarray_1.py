8b92259a7261dac0b192090c3e899c7f7ae65834
袁方 <fang.yuan@baishancloud.com>
2020-10-08 09:32:02 +0800

    new_coder: 718_maximum_length_of_repeated_subarray_1: ascend

--- new_coder/718_maximum_length_of_repeated_subarray_1.py
+++ new_coder/718_maximum_length_of_repeated_subarray_1.py
@@ -16,3 +16,15 @@ class Solution:
                 ans = max(ans, dp[i][j])
 
         return ans
+
+    def findLengthDpAscend(self, A: List[int], B: List[int]) -> int:
+        length_A, length_B = len(A), len(B)
+        ans = 0
+        dp = [[0] * (length_A + 1) for _ in range(length_B + 1)]
+
+        for i in range(1, length_A + 1):
+            for j in range(1, length_B + 1):
+                dp[i][j] = dp[i-1][j-1] + 1 if A[i-1] == B[j-1] else 0
+                ans = max(ans, dp[i][j])
+
+        return ans
