4523bbd2dfa92653b72d6eec2f9e87f11d2c9bd5
袁方 <fang.yuan@baishancloud.com>
2020-10-10 00:02:44 +0800

    344_reverse_string

---
+++ new_coder/344_reverse_string.py
@@ -0,0 +1,16 @@
+# link: https://leetcode-cn.com/problems/reverse-string/description/
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        if not s:
+            return
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
