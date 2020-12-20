b1ff9e2bc9f2e33c522c700e4fb003a333870ffc
袁方 <fang.yuan@baishancloud.com>
2020-12-20 09:12:16 +0800

    geek_time: 208_implement_trie: dict

--- geek_time/208_implement_trie.py
+++ geek_time/208_implement_trie.py
@@ -1,14 +1,69 @@
 # link: https://leetcode-cn.com/problems/implement-trie-prefix-tree/
 
 
+class TrieNodeDict:
+    def __init__(self, val="/"):
+        self.val = val
+        self.children = {}
+        self.is_end = False
+
+
+class TrieDict:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNodeDict()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        root = self.root
+        for c in word:
+            if c not in root.children:
+                new_node = TrieNodeDict(c)
+                root.children[c] = new_node
+            root = root.children[c]
+        root.is_end = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        root = self.root
+        for c in word:
+            if c not in root.children:
+                return False
+            root = root.children[c]
+        return root.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        root = self.root
+        for c in prefix:
+            if c not in root.children:
+                return False
+            root = root.children[c]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+
 class TrieNode:
     def __init__(self, val="/"):
         self.val = val
         self.children = [None] * 26
         self.is_end = False
 
-class Trie:
 
+class Trie:
     def __init__(self):
         """
         Initialize your data structure here.
@@ -54,7 +109,7 @@ class Trie:
 
 
 if __name__ == "__main__":
-    trie = Trie()
+    trie = TrieDict()
     trie.insert("apple")
     print(trie.search("apple"))
     print(trie.search("app"))
