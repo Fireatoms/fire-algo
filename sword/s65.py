232fe4f74fbb86ec7d9a80459a6eb865ccf0eaf7
袁方 <fang.yuan@baishancloud.com>
2021-01-15 08:14:18 +0800

    leetcode: s65

---
+++ sword/s65.py
@@ -0,0 +1,16 @@
+# link: https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/
+
+
+class Solution:
+    def add(self, a: int, b: int) -> int:
+        x = 0xffffffff
+        a, b = a & x, b & x
+        while b != 0:
+            a, b = a ^ b, (a & b) << 1 & x
+
+        return a if a <= 0x7ffffff else ~(a ^ x)
+
+
+if __name__ == "__main__":
+    sl = Solution()
+    print(sl.add(1, -99))
